74590e92d35fb284eb4b0e0afd77adb9199f98d1
auliaiskandar <iskandar.bin.rusliahmad@gmail.com>
2016-11-21 23:06:25 +0700

    Praktikum Komunikasi UDP

---
+++ Praktikum Komunikasi UDP/Praktikum Komunikasi UDP/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.buttonLedOn = new System.Windows.Forms.Button();
+            this.buttonLedOff = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxTemp = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(92, 26);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(151, 20);
+            this.textBoxIP.TabIndex = 2;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(92, 68);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(151, 20);
+            this.textBoxPort.TabIndex = 3;
+            // 
+            // buttonLedOn
+            // 
+            this.buttonLedOn.Location = new System.Drawing.Point(18, 109);
+            this.buttonLedOn.Name = "buttonLedOn";
+            this.buttonLedOn.Size = new System.Drawing.Size(108, 23);
+            this.buttonLedOn.TabIndex = 4;
+            this.buttonLedOn.Text = "LED On";
+            this.buttonLedOn.UseVisualStyleBackColor = true;
+            this.buttonLedOn.Click += new System.EventHandler(this.buttonLedOn_Click);
+            // 
+            // buttonLedOff
+            // 
+            this.buttonLedOff.Location = new System.Drawing.Point(134, 109);
+            this.buttonLedOff.Name = "buttonLedOff";
+            this.buttonLedOff.Size = new System.Drawing.Size(109, 23);
+            this.buttonLedOff.TabIndex = 5;
+            this.buttonLedOff.Text = "LED Off";
+            this.buttonLedOff.UseVisualStyleBackColor = true;
+            this.buttonLedOff.Click += new System.EventHandler(this.buttonLedOff_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Temperature";
+            // 
+            // textBoxTemp
+            // 
+            this.textBoxTemp.Location = new System.Drawing.Point(92, 36);
+            this.textBoxTemp.Name = "textBoxTemp";
+            this.textBoxTemp.Size = new System.Drawing.Size(151, 20);
+            this.textBoxTemp.TabIndex = 7;
+            this.textBoxTemp.TextChanged += new System.EventHandler(this.textBoxTemp_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxIP);
+            this.groupBox1.Controls.Add(this.buttonLedOff);
+            this.groupBox1.Controls.Add(this.buttonLedOn);
+            this.groupBox1.Controls.Add(this.textBoxPort);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(257, 165);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Send";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBoxTemp);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 190);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(257, 80);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Receive";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 281);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Button buttonLedOn;
+        private System.Windows.Forms.Button buttonLedOff;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxTemp;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
